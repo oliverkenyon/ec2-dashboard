f92861ecbcf8714a2df3e4807c1dd7fe8009704b
Oliver Kenyon <oliverkenyon@hotmail.com>
2019-12-12 16:19:52 +0000

    Fix hard-coded api url

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-// This is left hard-coded for now due to time constraints, but I would implement it 
-// using something like this: https://stackoverflow.com/a/40287063
-// As it should be configurable per environment
-const API_BASE = 'http://localhost:3000/api/v1'
+import { environment } from './../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +10,6 @@ export class ApiService {
   constructor(private httpClient: HttpClient) { }
 
   public getInstances(searchValue: String) {
-    return this.httpClient.get(`${API_BASE}/instances?searchValue=${searchValue}`);
+    return this.httpClient.get(`${environment.api_base}/instances?searchValue=${searchValue}`);
   }
 }
