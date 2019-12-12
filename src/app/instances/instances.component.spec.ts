83df2d4675bcb453f6f6ad857ec9a4a03cb338c7
Oliver Kenyon <oliverkenyon@hotmail.com>
2019-12-12 14:11:24 +0000

    Improve instance component test

--- src/app/instances/instances.component.spec.ts
+++ src/app/instances/instances.component.spec.ts
@@ -1,23 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpRequest } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 import { InstancesComponent } from './instances.component';
+import { ApiService } from '../api.service';
+
+const mockInstance = {
+  name: 'mock instance name',
+  id: 'mock instance id',
+  ips: {public: 'public ip', private: ['private ip']}
+};
 
 describe('InstancesComponent', () => {
   let component: InstancesComponent;
   let fixture: ComponentFixture<InstancesComponent>;
-
-  let httpTestingController: HttpTestingController;
-
-  const mockInstance = {
-  };
+  let mockApi: ApiService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -30,50 +33,46 @@ describe('InstancesComponent', () => {
         MatPaginatorModule,
         MatInputModule,
         MatFormFieldModule]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InstancesComponent);
-    httpTestingController = TestBed.get(HttpTestingController);
-
     component = fixture.componentInstance;
+
+    mockApi = TestBed.get(ApiService);
+    spyOn(mockApi, 'getInstances').and.returnValue(of([mockInstance]))
+
     fixture.detectChanges();
   });
 
-  it('should request list of instances on load with no search', async(() => {
-
+  it('should create', async(() => {
     expect(component).toBeTruthy();
+  }));
 
-    const mockApiResponse = [
-      mockInstance,
-      mockInstance
-    ];
-
-    const request = httpTestingController.expectOne(
-      (req: HttpRequest<any>) => req.urlWithParams.includes("/instances?searchValue="));
-
-    fixture.detectChanges();
+  it('should request list of instances on load with no search', async(() => {
     fixture.whenStable().then(() => {
-      request.flush(mockApiResponse);
-      httpTestingController.verify();
+      expect(mockApi.getInstances).toHaveBeenCalledWith("");
     })
   }));
 
   it('should send search requests back to API', async(() => {
-    const mockApiResponse = [
-      mockInstance,
-      mockInstance
-    ];
-
-    fixture.detectChanges();
     fixture.whenStable().then(() => {
       component.search("test")
-      const matches = httpTestingController.match(
-        (req: HttpRequest<any>) => req.urlWithParams.includes('/instances?searchValue=test'));
+      expect(mockApi.getInstances).toHaveBeenCalledWith("test");
+    });
+  }));
 
-      expect(matches.length).toEqual(1);
+  it('should render data from API', async(() => {
+    fixture.whenStable().then(() => {
+      const rows = fixture.debugElement.nativeElement.querySelectorAll('.mat-row');
+      expect(rows.length).toBe(1);
+
+      const rowContent = rows[0].textContent;
+      expect(rowContent).toContain('mock instance name');
+      expect(rowContent).toContain('mock instance id');
+      expect(rowContent).toContain('public ip');
+      expect(rowContent).toContain('private ip');
     });
   }));
 
